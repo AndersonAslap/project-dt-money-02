f71812e9212055f03925006a79949bbecd89994e
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-01-02 17:48:50 -0300

    feat: table transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,52 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TranasactionsContainer, TransactionsTable } from "./styles";
 
 export function Tranasactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TranasactionsContainer >
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.800,80
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Jogos</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 12.800,80
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Alimentação</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.800,80
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+
+                    </tbody>
+                </TransactionsTable>
+            </TranasactionsContainer>
         </div>
     )
 }
