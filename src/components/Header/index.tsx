9b15cefbac824aa6c81de6648d96aa575a9c9e03
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-01-03 11:02:08 -0300

    feat: layouts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -8,10 +10,17 @@ export function Header() {
             <HeaderContent>
                 <img src={logoImg} alt="" />
 
-                <NewTransactionButton>
-                    Nova transação
-                </NewTransactionButton>
-            </HeaderContent>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>
+                            Nova transação
+                        </NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewTransactionModal />
+                </Dialog.Root>
+
+        </HeaderContent>
         </HeaderContainer>
     )
 }
